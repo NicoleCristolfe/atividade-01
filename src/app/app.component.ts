fe2228e8868b9537850ab44bae0fdf46c6ee54c1
Nicole Cristolfe <nicole.silva@labldi.com.br>
2024-09-05 17:09:33 -0300

    [main] - Cinema-Lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FilmesComponent } from './filmes/filmes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FilmesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'atividade-1';
 }
+@NgModule ({
+  declarations: [],
+  
+  imports:[AppComponent, FilmesComponent],
+  })
+  
+  export class AppModule {}
---
+++ src/app/filmes/filmes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filmes',
+  standalone: true,
+  imports: [],
+  templateUrl: './filmes.component.html',
+  styleUrl: './filmes.component.scss'
+})
+export class FilmesComponent {
+  filme={
+    infos:{
+      filme1:'Carandiru'+ '(2003)',
+      filme2: 'Cidade de Deus' + '(2002)',
+      filme3: 'Estômago' + '(2007)',
+      filme4: 'A Hora da Estrela' + '(1985)'
+
+    }
+}}
